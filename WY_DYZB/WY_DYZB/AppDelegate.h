97ac89276a2ba94a92469dedec5f7ab0f221640b
apple <apple@appledemacbook-pro-2.local>
2016-10-20 11:27:24 +0800

    初始化项目

---
+++ WY_DYZB/WY_DYZB/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  WY_DYZB
+//
+//  Created by apple on 16/10/20.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ WY_DYZB/WY_DYZB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WY_DYZB
+//
+//  Created by apple on 16/10/20.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
